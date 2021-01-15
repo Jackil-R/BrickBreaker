479052247a621ce949a69c764e20e8cda791cc3d
Jackil-R <jackil_rajnicant@hotmail.co.uk>
2021-01-15 21:38:54 +0000

    add collision and ball moving mechanism

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -2,18 +2,56 @@ import java.awt.*;
 
 public class Ball extends Rectangle{
 
+    private int xdir;
+    private int ydir;
+
     public Ball(int x, int y){
         this.x = x;
         this.y = y;
         this.width=5;
         this.height=5;
+        xdir = 1;
+        ydir = -1;
 
     }
 
     public void paint(Graphics g){
-        g.setColor(Color.BLACK);
+        g.setColor(Color.RED);
         g.drawOval(x, y, width, width);
         g.fillOval(x, y, width, width);
     }
 
+    void move() {
+
+        x += xdir;
+        y += ydir;
+
+        if (x == 0) {
+            //System.out.println(0);
+            setXDir(1);
+
+        }
+
+        if (x == Commons.WIDTH.getValue() - 5) {
+            //System.out.println(1);
+            setXDir(-1);
+        }
+
+        if (y == 0) {
+            //System.out.println(2);
+            setYDir(1);
+        }
+    }
+
+    public void setYDir(int ydir) {
+        this.ydir = ydir;
+    }
+
+    public void setXDir(int xdir) {
+        this.xdir = xdir;
+    }
+
+    public int getYDir() {
+        return ydir;
+    }
 }
--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -4,52 +4,61 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
     private Paddle paddle;
     private Timer timer;
-    private Brick[] bricks;
+    private List<Brick> bricks;
     private Ball ball;
+    private String endGameMessage = "Game Over";
+    private boolean inGame = true;
+
 
     public GamePanel() {
         setFocusable(true);
         setPreferredSize(new Dimension(Commons.WIDTH.getValue(), Commons.HEIGHT.getValue()));
         paddle = new Paddle(Commons.INIT_PADDLE_X.getValue(), Commons.INIT_PADDLE_Y.getValue());
+        ball = new Ball(Commons.INIT_BALL_X.getValue(), Commons.INIT_BALL_Y.getValue());
         addKeyListener(this);
-        bricks = new Brick[Commons.N_OF_BRICKS.getValue()];
+        bricks = new ArrayList<Brick>();
+
         int k = 0;
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 6; j++) {
-                bricks[k] = new Brick(j * 40 + 30, i * 10 + 50);
+                bricks.add(k,new Brick(j * 40 + 30, i * 10 + 50));
                 k++;
             }
         }
-        timer = new Timer(10, this);
+        timer = new Timer(Commons.PERIOD.getValue(), this);
         timer.start();
 
     }
 
     private void stopGame() {
-
+        inGame = false;
         timer.stop();
     }
 
     public void paint(Graphics g) {
+        ball.paint(g);
         paddle.paint(g);
-        for (int i = 0; i < Commons.N_OF_BRICKS.getValue(); i++) {
-            if (!bricks[i].isDestroyed()) {
-                bricks[i].paint(g);
-            }
+        for (int i = 0; i < bricks.size(); i++) {
+            bricks.get(i).paint(g);
         }
-    }
 
+        if(!inGame){
+           super.paint(g);
+           gameFinished(g);
+        }
+        Toolkit.getDefaultToolkit().sync();
+    }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
+    public void keyTyped(KeyEvent e) { }
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -63,7 +72,77 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        ball.move();
         paddle.move();
+        checkCollision();
         this.repaint();
+    }
+
+    public void checkCollision(){
+
+        if (ball.getMaxY() > Commons.BOTTOM_EDGE.getValue()) {
+            endGame("Game Over");
+        }
+
+        if(ball.isEmpty()){
+            endGame("Victory");
+        }
+
+        if (ball.intersects(paddle)) {
+
+            int paddlePosition = (int) paddle.getMinX();
+            int ballLPosition = (int) ball.getMinX();
+
+            int firstSplit = paddlePosition + 8;
+            int secondSplit = paddlePosition + 16;
+            int thirdSplit = paddlePosition + 24;
+            int fourthSplit = paddlePosition + 32;
+
+            if (ballLPosition < firstSplit) {
+                ball.setXDir(-1);
+                ball.setYDir(-1);
+            }
+            if (ballLPosition >= firstSplit && ballLPosition < secondSplit) {
+                ball.setXDir(-1);
+                ball.setYDir(-1 * ball.getYDir());
+            }
+            if (ballLPosition >= secondSplit && ballLPosition < thirdSplit) {
+                ball.setXDir(0);
+                ball.setYDir(-1);
+            }
+            if (ballLPosition >= thirdSplit && ballLPosition < fourthSplit) {
+                ball.setXDir(1);
+                ball.setYDir(-1 * ball.getYDir());
+            }
+            if (ballLPosition > fourthSplit) {
+                ball.setXDir(1);
+                ball.setYDir(-1);
+            }
+        }
+
+        for(int i=0; i<bricks.size(); i++){
+            if(ball.intersects(bricks.get(i))){
+                ball.setYDir(-1 * ball.getYDir());
+                bricks.remove(i);
+            }
+        }
+
+
+    }
+
+    private void gameFinished(Graphics g) {
+
+        var font = new Font("Verdana", Font.BOLD, 18);
+        FontMetrics fontMetrics = this.getFontMetrics(font);
+
+        g.setColor(Color.BLACK);
+        g.setFont(font);
+        g.drawString(endGameMessage,
+                (Commons.WIDTH.getValue() - fontMetrics.stringWidth(endGameMessage)) / 2, Commons.WIDTH.getValue() / 2);
+    }
+
+    private void endGame(String message) {
+        stopGame();
+        endGameMessage = message;
     }
 }
